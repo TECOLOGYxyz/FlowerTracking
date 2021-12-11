1d5aae82e041e450ce2b2116057bbc7dc16a15f5
Hjalte Mann <mann@bios.au.dk>
2021-12-11 14:51:05 +0100

    Experiment run

--- code/track.py
+++ code/track.py
@@ -35,10 +35,10 @@ br = "\n"
 
 verbose = True # Set to True if you want tracking process printed to screen and False if not
 
-prefix_results_filename = "5testing_THUL-01"
+prefix_results_filename = "2testing_NARS-13"
 
 list_max_disappeared = [4] # Maximum number of frames a track can be lost before new points will be forces into a new track.
-list_running_mean_threshold = [5] # Maximum number of frames for calculating the running mean of the position of an object If there are less than this number of frames currently in the track, a mean over what is in the track will be used..
+list_running_mean_threshold = [0] # Maximum number of frames for calculating the running mean of the position of an object If there are less than this number of frames currently in the track, a mean over what is in the track will be used..
 list_max_distance = [0] # If set to 0, this parameter will be ignored. If not zero, points that have a distance to tracked objects higher than this parameter will be forced into new tracks.
 
 list_of_parameters = [(x,y,z) for x in list_max_disappeared for y in list_running_mean_threshold for z in list_max_distance] # A list of all combinations of the above parameters.
@@ -46,7 +46,7 @@ list_of_parameters = [(x,y,z) for x in list_max_disappeared for y in list_runnin
 
 #### PATH TO DETECTIONS ####
 #detections = pd.read_csv(r'../Dummy_fortracking2.csv')
-detections = pd.read_csv(r"U:\BITCue\Projekter\TrackingFlowers\data\annotations\2020_05_15_NorwayAnnotations_THUL-01_IndividualAnnotations_FRCNN_Metrics.csv")
+detections = pd.read_csv(r"U:\BITCue\Projekter\TrackingFlowers\data\annotations\2020_04_30_NorwayAnnotations_NARS-13_IndividualAnnotations_FRCNN_Metrics.csv")
 detections['frame'] = detections['filename'].str.extract('(\d{6})')
 detections['x_c'] = (detections['x_min'] + detections['x_max']) / 2
 detections['y_c'] = (detections['y_min'] + detections['y_max']) / 2
@@ -252,11 +252,12 @@ for i in list_of_parameters:
     
     for f in frames:
         t.track(f)
+    endtime = time.time()
+    print(f'Tracking done. That took {round(endtime-starttime, 3)} seconds. That is {round((endtime-starttime)/len(frames), 3)} seconds per frame.')
     t.write_tracks_file()
     tracks = pd.read_csv(resultsFilename)
     print(f'maxDis: {i[0]}, runMean: {i[1]} - Number of tracks found: {len(tracks.objectID.unique())}')
-endtime = time.time()
-print(f'Tracking done. That took {round(endtime-starttime, 3)} seconds.')
+
 # =============================================================================
 
 
