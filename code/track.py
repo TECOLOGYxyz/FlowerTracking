d696f10935f3e161572d2610e97009b1e8bc6f90
Hjalte Mann <mann@bios.au.dk>
2021-12-14 15:37:08 +0100

    Minor updates

--- code/track.py
+++ code/track.py
@@ -33,38 +33,15 @@ br = "\n"
 
 verbose = False # Set to True if you want tracking process printed to screen and False if not
 
-prefix_results_filename = "parameterTest_1_NYAA-04"
-
-list_max_disappeared = [0,5,10,15,20,25,30] # Maximum number of frames a track can be lost before new points will be forces into a new track.
-list_running_mean_threshold = [0,5,10,15,20,25,30] # Maximum number of frames for calculating the running mean of the position of an object If there are less than this number of frames currently in the track, a mean over what is in the track will be used..
-list_max_distance = [0,100,200,300,400,500] # If set to 0, this parameter will be ignored. If not zero, points that have a distance to tracked objects higher than this parameter will be forced into new tracks.
-
-list_of_parameters = [(x,y,z) for x in list_max_disappeared for y in list_running_mean_threshold for z in list_max_distance] # A list of all combinations of the above parameters.
-
-
-#### PATH TO DETECTIONS ####
-#detections = pd.read_csv(r'../Dummy_fortracking2.csv')
-
-detections = pd.read_csv(r"U:\BITCue\Projekter\TrackingFlowers\data\annotations\2021_12_13_NorwayAnnotations_NYAA-04_IndividualAnnotations_FRCNN_Metrics.csv")
-detections['frame'] = detections['filename'].str.extract('(\d{6})')
-detections['x_c'] = (detections['x_min'] + detections['x_max']) / 2
-detections['y_c'] = (detections['y_min'] + detections['y_max']) / 2
-detections['frame'] = detections['frame'].astype('int')
-
-frames = list(set(detections['frame'].tolist()))
-frames = sorted([int(i) for i in frames])
-
-if verbose:
-    print("Here are the detections",br,detections,br)
-
 class tracker():
-    def __init__(self, max_disappeared, max_distance, running_mean_threshold, results_filename, frames):
+    def __init__(self, max_disappeared, max_distance, running_mean_threshold, results_filename, frames, detections, verbose):
         self.nextObjectID = 0 # Counter for object ids
         self.objects = OrderedDict() # Dictionary. objectID is the key, centroid is the content
         self.means = OrderedDict() # Dictionary to keep track of running means of object coordinates
         
         self.disappeared = OrderedDict() # Keeps track of how long an objectID has been lost
 
+        self.detections = detections
         self.max_disappeared = max_disappeared # store parameters for use in the class
         self.max_distance = max_distance
         self.running_mean_threshold = running_mean_threshold
@@ -77,6 +54,11 @@ class tracker():
             header = 'frame,filename,x_min,x_max,y_min,y_max,x_c,y_c,objectID\n'
             resultFile.write(header)
 
+
+        if verbose:
+            print("Here are the detections",br,self.detections,br)
+    
+    
     def store_tracking_results(self, frame, centroid, objectID):
         self.tracks.append([frame, centroid[0], centroid[1], objectID])
         if verbose:
@@ -88,18 +70,18 @@ class tracker():
         with open(self.results_filename, 'a') as resultFile:
             for t in self.tracks:
                 frame, x_c, y_c, objectID = t[0], t[1], t[2], t[3]
-                filename = detections.loc[detections['frame'] == frame, 'filename'].iloc[0]
-                x_min = detections.loc[((detections['frame'] == frame) & (detections['x_c'] == x_c) & (detections['y_c'] == y_c)), 'x_min'].iloc[0]
-                x_max = detections.loc[((detections['frame'] == frame) & (detections['x_c'] == x_c) & (detections['y_c'] == y_c)), 'x_max'].iloc[0]
-                y_min = detections.loc[((detections['frame'] == frame) & (detections['x_c'] == x_c) & (detections['y_c'] == y_c)), 'y_min'].iloc[0]
-                y_max = detections.loc[((detections['frame'] == frame) & (detections['x_c'] == x_c) & (detections['y_c'] == y_c)), 'y_max'].iloc[0]
+                filename = self.detections.loc[self.detections['frame'] == frame, 'filename'].iloc[0]
+                x_min = self.detections.loc[((self.detections['frame'] == frame) & (self.detections['x_c'] == x_c) & (self.detections['y_c'] == y_c)), 'x_min'].iloc[0]
+                x_max = self.detections.loc[((self.detections['frame'] == frame) & (self.detections['x_c'] == x_c) & (self.detections['y_c'] == y_c)), 'x_max'].iloc[0]
+                y_min = self.detections.loc[((self.detections['frame'] == frame) & (self.detections['x_c'] == x_c) & (self.detections['y_c'] == y_c)), 'y_min'].iloc[0]
+                y_max = self.detections.loc[((self.detections['frame'] == frame) & (self.detections['x_c'] == x_c) & (self.detections['y_c'] == y_c)), 'y_max'].iloc[0]
                 
                 resultFile.write(f'{frame},{filename},{x_min},{x_max},{y_min},{y_max},{x_c},{y_c},{objectID}{br}')
         endtime = time.time()
         print(f'Writing done. That took {round(endtime-starttime, 4)} seconds. {br}File saved as: {self.results_filename}{br}')
 
     def get_frame_detections(self, frame):
-        block = detections.loc[detections['frame'] == frame]
+        block = self.detections.loc[self.detections['frame'] == frame]
         frame_detections = block[["x_c", "y_c"]] # We just need the centroid, so we'll grab that and return it
         return frame_detections
 
@@ -242,24 +224,7 @@ class tracker():
 # currentTime=('%02d-%02d-%02d'%(currentTime.hour,currentTime.minute,currentTime.second))
 
 
-# ===================== Run tracking on several combinations of parameters ====
-if __name__ == "__main__":
-    verbose = True # Set to True if you want tracking process printed to screen and False if not
-    starttime = time.time()
-    for i in list_of_parameters:
-        resultsFilename = f'../testResults/{prefix_results_filename}_maxDisap_{i[0]}_runMean_{i[1]}_maxDist_{i[2]}.csv'    
-    
-        t = tracker(i[0], i[2], i[1], resultsFilename, frames)
-    
-        for f in frames:
-            t.track(f)
-        endtime = time.time()
-        print(f'Tracking done. That took {round(endtime-starttime, 3)} seconds. That is {round((endtime-starttime)/len(frames), 3)} seconds per frame.')
-        t.write_tracks_file()
-        tracks = pd.read_csv(resultsFilename)
-        print(f'maxDis: {i[0]}, runMean: {i[1]} - Number of tracks found: {len(tracks.objectID.unique())}')
 
-# =============================================================================
 
 
 
